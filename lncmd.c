2bc3272ea08f445391e968272b4c4b45c49b0769
sumanlokesh <sumanlokesh.dev@gmail.com>
2022-02-06 19:02:06 +0530

    Changed extension to C

--- lncmd.c
+++ lncmd.c
@@ -1,11 +1,9 @@
-#include<iostream>
 #include<stdio.h>
 #include<sys/stat.h>
 #include<unistd.h>
 #include<fcntl.h>
 #include<string.h>
 #include<stdlib.h>
-using namespace std;
 int main(int argc, char * argv[])
 {
     if(argc != 3 || argc !=4 || argv[1] == "--help")
