a2dc1e66799aa2574d1ef319fa446638cd7e12bc
sumanlokesh <sumanlokesh.dev@gmail.com>
2022-02-06 19:23:05 +0530

    Conversion of everything to C files

--- lncmd.c
+++ lncmd.c
@@ -6,7 +6,7 @@
 #include<stdlib.h>
 int main(int argc, char * argv[])
 {
-    if(argc != 3 || argc !=4 || argv[1] == "--help")
+    if(argc < 3 || argc > 4 || argv[1] == "--help")
 	{
 		printf("Usage: lncmd [-s] source_file destination_file, use -s for symbolic link, default: hard link\n");
 		exit(EXIT_FAILURE);
